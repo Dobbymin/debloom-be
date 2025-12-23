baf8f07e52827a987cafdb9708cc4fce450f9c9c
Dobbymin <gmin990624@gmail.com>
2025-12-24 02:54:02 +0900

    feat: 개발 서버 주소 변경

--- swagger/config.js
+++ swagger/config.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'https://debloom-be.vercel.app/swagger-ui/',
+        url: 'https://debloom-be.vercel.app/',
         description: 'Deployed Server',
       },
       {

f0c0051f421ac88506105054947bad98f272464c
Dobbymin <gmin990624@gmail.com>
2025-12-24 00:19:10 +0900

    feat: 라우터 설청 추가

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,24 @@
-import { handlerTodos } from '../apis/todos/controller/todos.controller.js';
+import {
+  createTodoGroupHandler,
+  createTodoHandler,
+  handlerTodos,
+  handlerTodosCountByMonth,
+  updateTodoHandler,
+} from '../apis/todos/controller/todos.controller.js';
 
 export const router = (app) => {
+  // GET: 모든 todos 조회
   app.get('/api/todos', handlerTodos);
+
+  // GET: 월별 날짜별 todo 갯수 조회
+  app.get('/api/todos/monthly', handlerTodosCountByMonth);
+
+  // POST: 새로운 todo 추가
+  app.post('/api/todos', createTodoHandler);
+
+  // PUT: 기존 todo 수정
+  app.put('/api/todos/:id', updateTodoHandler);
+
+  // POST: 새로운 todo_group(날짜별 카테고리) 생성
+  app.post('/api/todos/groups', createTodoGroupHandler);
 };
